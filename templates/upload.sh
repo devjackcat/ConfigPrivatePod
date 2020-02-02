f5de282a35f9e6c0867026adba1377635d152044
永平 <yongping@anve.com>
2020-02-02 15:30:38 +0800

    更新upload.sh

--- templates/upload.sh
+++ templates/upload.sh
@@ -11,6 +11,8 @@ set -e # 遇到错误时终止当前脚本执行
 #LineNumber=`grep -nE 's.version.*=' JCTheme.podspec | cut -d : -f1`
 #sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
 
+gitsource=https://gitee.com/devjackcat/JCS_PodSpecs.git,https://cdn.cocoapods.org/
+
 # 采集Tag
 tag=""
 getTagName() {
@@ -22,7 +24,7 @@ getTagName() {
 getTagName
 
 # 验证podspec文件是否合法
-pod lib lint __ProjectName__.podspec
+pod lib lint __ProjectName__.podspec --sources=${gitsource}  --verbose --allow-warnings --use-libraries
 
 LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
 sed -i "" "${LineNumber}s/^\([ ]*s.version[ ]*=[ ]*\"\)\(.*\)\(\"[ ]*\)$/\1${tag}\3/g" __ProjectName__.podspec
@@ -32,7 +34,7 @@ git commit -am ${tag}
 git tag ${tag}
 git push origin master --tags
 # 提交到私有库
-#pod repo push PrivatePods __ProjectName__.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+pod repo push JCS_PodSpecs __ProjectName__.podspec --sources=${gitsource} --verbose --allow-warnings --use-libraries --use-modular-headers
 #提交到公开库
-pod trunk push __ProjectName__.podspec --verbose --allow-warnings --use-libraries
+#pod trunk push __ProjectName__.podspec --verbose --allow-warnings --use-libraries
 
